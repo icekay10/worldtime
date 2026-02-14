3844c7317476ec710bfb0200eabcde921c96e92f
icekay10 <isatakamara1001@gmail.com>
2026-02-14 22:00:17 +0200

    recent

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,6 @@
+"use strict";(()=>{var a={};a.id=220,a.ids=[220],a.modules={361:a=>{a.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},2015:a=>{a.exports=require("react")},3873:a=>{a.exports=require("path")},3988:(a,b,c)=>{c.r(b),c.d(b,{default:()=>f});var d=c(8732),e=c(2341);function f(){return(0,d.jsxs)(e.Html,{lang:"en",children:[(0,d.jsxs)(e.Head,{children:[(0,d.jsx)("script",{async:!0,src:"https://www.googletagmanager.com/gtag/js?id=G-QJDMC5FK42"}),(0,d.jsx)("script",{dangerouslySetInnerHTML:{__html:`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QJDMC5FK42');
+            `}})]}),(0,d.jsxs)("body",{children:[(0,d.jsx)(e.Main,{}),(0,d.jsx)(e.NextScript,{})]})]})}},8732:a=>{a.exports=require("react/jsx-runtime")}};var b=require("../webpack-runtime.js");b.C(a);var c=b.X(0,[238,341],()=>b(b.s=3988));module.exports=c})();
